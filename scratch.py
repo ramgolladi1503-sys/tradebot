c5c8d3ed2a8a66ad0a4399423232a931bab73a42
Ramgolladi <ramgolladi1503@gmail.com>
2026-06-14 04:40:35 +0530

    Clean up garbage files to pass code excellence gates

--- scratch.py
+++
@@ -1,7 +0,0 @@
-from autobahn.twisted.websocket import WebSocketClientProtocol
-p = WebSocketClientProtocol()
-p.state = WebSocketClientProtocol.STATE_CONNECTING
-try:
-    p.sendMessage(b"test")
-except Exception as e:
-    print("Exception:", type(e).__name__, e)
