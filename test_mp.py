69eca99fef58cedff7f7527b3a90c5d1c7b5d66c
Ramgolladi <ramgolladi1503@gmail.com>
2026-06-22 09:22:54 +0530

    chore: remove accidentally tracked untracked files

--- test_mp.py
+++
@@ -1,5 +0,0 @@
-from _pytest.monkeypatch import MonkeyPatch
-mp = MonkeyPatch()
-class C: pass
-mp.setattr(C, 'new_attr', True, raising=False)
-print(getattr(C, 'new_attr', False))
--- test_mp2.py
+++
@@ -1,5 +0,0 @@
-from _pytest.monkeypatch import MonkeyPatch
-mp = MonkeyPatch()
-mp.setattr("config.config.NEW_ATTR", True, raising=False)
-import config.config
-print(getattr(config.config, 'NEW_ATTR', False))
