6daf14195ac170323eecafa2c4a1200d5b3e848d
Ramgolladi <ramgolladi1503@gmail.com>
2026-07-07 03:34:45 +0530

    test: fix indentation in test_historical_data_fetch_plan.py

--- tests/test_historical_data_fetch_plan.py
+++ tests/test_historical_data_fetch_plan.py
@@ -8,9 +8,8 @@ def test_historical_data_fetch_plan():
         with open(plan_path, "r") as f:
             data = json.load(f)
         _len = len(data.get("chunks", []))
-    assert _len > 0
+        assert _len > 0
         assert data.get("estimated_calls", 0) > 0
-
 
 def test_dummy():
     assert 1 != 2
