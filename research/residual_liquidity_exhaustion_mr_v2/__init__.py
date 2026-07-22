6d98f3ddcf564b46e4bf7a479f964a17dc2fb6d7
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-07-23 01:47:01 +0530

    research: add residual mean-reversion atlas package

---
+++ research/residual_liquidity_exhaustion_mr_v2/__init__.py
@@ -0,0 +1,19 @@
+from .pattern_atlas import (
+    PatternAtlasContract,
+    build_residual_panel,
+    build_segment_metrics,
+    canonicalize_symbol,
+    extract_residual_events,
+    permutation_control,
+    resample_completed_bars,
+)
+
+__all__ = [
+    "PatternAtlasContract",
+    "build_residual_panel",
+    "build_segment_metrics",
+    "canonicalize_symbol",
+    "extract_residual_events",
+    "permutation_control",
+    "resample_completed_bars",
+]
