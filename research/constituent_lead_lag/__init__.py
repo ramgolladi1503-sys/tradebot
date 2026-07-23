b55906748273cd5272842c8b081ddcb3120667a9
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-07-23 13:14:12 +0530

    research: export unweighted breadth API

--- research/constituent_lead_lag/__init__.py
+++ research/constituent_lead_lag/__init__.py
@@ -11,15 +11,35 @@ from .model import (
     validate_bars,
     validate_weights,
 )
+from .unweighted import (
+    UnweightedSignalState,
+    UnweightedThresholds,
+    chronological_fold_summary,
+    classify_unweighted_state,
+    evaluate_unweighted_first_signal_per_session,
+    generate_unweighted_signal_states,
+    select_universe_snapshot,
+    summarize_unweighted_outcomes,
+    validate_universe,
+)
 
 __all__ = [
     "DataContractError",
     "SignalState",
     "StrategyThresholds",
     "TradeOutcome",
+    "UnweightedSignalState",
+    "UnweightedThresholds",
+    "chronological_fold_summary",
+    "classify_unweighted_state",
     "evaluate_first_signal_per_session",
+    "evaluate_unweighted_first_signal_per_session",
     "generate_signal_states",
+    "generate_unweighted_signal_states",
+    "select_universe_snapshot",
     "summarize_outcomes",
+    "summarize_unweighted_outcomes",
     "validate_bars",
+    "validate_universe",
     "validate_weights",
 ]
