75537d7b7ed336c41510f339a5f3e8527d0f7e03
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-08-03 23:30:55 +0530

    feat: export PR763 reliability certification

--- core/ai_reliability_agent/__init__.py
+++ core/ai_reliability_agent/__init__.py
@@ -2,17 +2,26 @@
 
 from .agent import AssertionVerifier, ReliabilityAgent, ScriptedReasoner, ToolRegistry
 from .analytics import analyze_candidates, build_candidate_autopsy, derive_session_verdict
+from .certification import run_component_certification
 from .contracts import *  # noqa: F401,F403
 from .evidence import EvidenceLedger
 from .openai_reasoner import OpenAIReasoner
+from .pr763_session import (
+    FAILED_VERDICT as PR763_FAILED_VERDICT,
+    PASS_VERDICT as PR763_PASS_VERDICT,
+    PENDING_VERDICT as PR763_PENDING_VERDICT,
+    certify_pr763_session,
+)
 from .runtime import build_session_manifest, build_tools, finalize_session
-from .certification import run_component_certification
 from .supervisor import LiveAgentSupervisor, detect_triggers
 
 __all__ = [
     "AssertionVerifier",
     "EvidenceLedger",
     "OpenAIReasoner",
+    "PR763_FAILED_VERDICT",
+    "PR763_PASS_VERDICT",
+    "PR763_PENDING_VERDICT",
     "ReliabilityAgent",
     "ScriptedReasoner",
     "ToolRegistry",
@@ -20,6 +29,7 @@ __all__ = [
     "build_candidate_autopsy",
     "build_session_manifest",
     "build_tools",
+    "certify_pr763_session",
     "derive_session_verdict",
     "finalize_session",
     "run_component_certification",
