cdc1fc242f4d99be4442bc4d5b9a99d2ca04e221
ramgolladi1503-sys <ramgolladi1503@gmail.com>
2026-09-07 11:03:44 +0530

    Expose AlphaTrend-inspired research API

---
+++ research/alphatrend_mechanism_v1/__init__.py
@@ -0,0 +1,23 @@
+from .engine import (
+    AlphaTrendMechanismConfig,
+    BEARISH,
+    BULLISH,
+    NEUTRAL,
+    SIGNAL_COLUMNS,
+    add_forward_labels,
+    build_features,
+    build_negative_controls,
+    evaluate_signal,
+)
+
+__all__ = [
+    "AlphaTrendMechanismConfig",
+    "BEARISH",
+    "BULLISH",
+    "NEUTRAL",
+    "SIGNAL_COLUMNS",
+    "add_forward_labels",
+    "build_features",
+    "build_negative_controls",
+    "evaluate_signal",
+]
